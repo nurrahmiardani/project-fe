be017f2355ddebfaa1d63db70168d03fa7ad295d
rehannnnnn <hannn9491@gmail.com>
2021-11-09 20:30:35 +0700

    Add Navbar

---
+++ anvbar/src/components/Navbar/Menuitems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Edukasi',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Transaksi',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Login',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
